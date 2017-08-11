16af39778fcdeb5e0d86f98feb11a6f92be23134
esobando <esobando92@gmail.com>
2017-08-11 17:45:55 -0400

    Here it is

--- app/src/main/java/com/example/admin/testproject/AddNewCarFragment.java
+++ app/src/main/java/com/example/admin/testproject/AddNewCarFragment.java
@@ -34,7 +34,7 @@ public class AddNewCarFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     public AddNewCarFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -95,18 +95,8 @@ public class AddNewCarFragment extends Fragment {
     }
 
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Car car);
     }
 }
